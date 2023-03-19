04ed69b7963e57df871131ef40362fd386fdeb38
React <3809164@gmail.com>
2023-03-19 19:35:15 +0200

    final

--- auth-react/src/Components/Home.jsx
+++ auth-react/src/Components/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            asasasasa
+        <div className={'flex flex-col justify-center text-center font-bold text-5xl'}>
+            <h1 className={'flex m-auto top-[50%] left-[50%]'}>Home Page</h1>
         </div>
     );
 };
